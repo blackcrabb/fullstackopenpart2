08f63a06717fbed602b509e2c9f6b89b904294bb
blackcrabb <niyati20002000@gmail.com>
2020-07-20 19:41:03 +0530

    complete 2.5

--- src/index.js
+++ src/index.js
@@ -1,60 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = (head) => {
-  return(
-    <>
-    <h1>{head.name}</h1>
-    </>
-  )
-}
-
-const Part = (par) => {
-  return(
-    <>
-    <p>{par.part} {par.exercise}</p>
-    </> 
-  )
-}
-
-const Content = (prop) => {
-  return(
-    <>
-    {
-      prop.parts.map((n)=>
-        <Part key={n.key} part={n.name} exercise={n.exercises} />
-      )}
-    </>
-  )
-}
-
-const Total = (tot) => {
-  const total = tot.parts.reduce((sum, parts)=>{
-    return sum + parts.exercises
-  },0)
-  return(
-    <>
-    <h3>total of {total} exercises</h3>
-    </>
-  )
-}
-
-const Course = (props) => {
-    return (
-      <div>
-      {
-        props.course.map((ex)=>
-        <React.Fragment>
-          <Header key={ex.id} name={ex.name} />
-          <Content key={ex.id} parts = {ex.parts} />
-          <Total key={ex.id} parts = {ex.parts}/>
-        </React.Fragment>
-         
-        )
-      }
-      </div>
-    )
-}
+import Course from './components/course'
 
 const App = () => {
   const course = [
