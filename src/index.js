52c8754b65e9d597cb0ce0fab014f47921740679
blackcrabb <niyati20002000@gmail.com>
2020-07-29 15:15:09 +0530

    step1

--- src/index.js
+++ src/index.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react'
 
+
 const App = () => {
+
   const [ persons, setPersons ] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    setPersons(persons.concat(event.target.value))
+  }
+
+  
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input 
+          value={newName}
+          onChange={handleNameChange}
+            />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" >add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <br/>
+        {persons.name}
     </div>
   )
 }
